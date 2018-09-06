f588eae86e400e3d9ae6dd2b623dd5db0dfb378c
Vlad Bashlak <st063745@student.spbu.ru>
2018-09-06 12:39:07 +0300

    Add idea project files

--- src/main/java/edu/spbu/sort/IntSort.java
+++ src/main/java/edu/spbu/sort/IntSort.java
@@ -8,9 +8,7 @@ import java.util.List;
  * Created by artemaliev on 07/09/15.
  */
 public class IntSort {
-  public static void sort (int array[]) {
-    Arrays.sort(array);
-  }
+  public static void sort (int array[]) {Arrays.sort(array);}
 
   public static void sort (List<Integer> list) {
     Collections.sort(list);
